400f7ecfbd8baa30f5e62c3a59a40a4c3eb50bf0
kenta.togashi <kenta.togashi@kddi-web.com>
2019-04-24 11:26:17 +0900

    Update settings

--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
-#!/bin/sh
-/usr/local/bin/uwsgi --ini uwsgi.ini
+#!/bin/bash
+
+if [ ! `which uwsgi` ]
+then
+        echo 'not found executable uwsgi'
+        exit 1
+fi
+
+export UWSGI_CHDIR=$(pwd)
+export UWSGI_EXEC=$(pwd)/uwsgi.py
+export UWSGI_LOG=$(pwd)/hoge.log
+echo $UWSGI_CHDIR $UWSGI_EXEC $UWSGI_LOG
+uwsgi --ini uwsgi.ini
